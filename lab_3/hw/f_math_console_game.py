98d8eb394c6bd7870f163b2ed3609ab4e4e78899
minh duc <minhduc.096.99@gmail.com>
2018-03-28 13:11:06 +0700

    upadte fmath

--- lab_3/hw/f_math_console_game.py
+++ lab_3/hw/f_math_console_game.py
@@ -21,16 +21,25 @@ while playing:
         err = randint(-1, 1)
         op = ['+', '-']
         ran_op = choice(op)
-    elif level >= 12:
-        if level == 12:
+    elif level <= 12:
+        if level == 8:
             print('level up!!!')
         x = randint(1, 30)
         y = randint(1, 30)
         err = randint(-1, 1)
-        op = ['+', '-', '*', '/']
+        op = ['+', '-', '*', '/', '*', '/']
         ran_op = choice(op)
-        if level == 20:
-            print('Win cmnr!!')
+    elif level <= 16:
+        if level == 12:
+            print('level up!!!')
+        x = randint(1, 40)
+        y = randint(1, 40)
+        err = randint(-1, 1)
+        op = ['+', '-', '*', '/', '*', '/','-','/','/']
+        ran_op = choice(op)
+    elif level == 17:
+        print('Win cmnr!!')
+        break
 
 
 
@@ -38,14 +47,14 @@ while playing:
 
 
 
-    print('*'*20,'{0} {1} {2} = {3}'.format(x, ran_op ,y, result), '*'*20)
+    print('\n','*'*20,'{0} {1} {2} = {3}'.format(x, ran_op ,y, result), '*'*20)
 
     answer = input('(Y/N)?').upper()
 
-
     if (err == 0 and answer == 'Y') or (err != 0 and answer == 'N'):
-        print('yayyy')
+        print('\n','*'*20, 'Ding Ding!!','*'*20)
         level += 1
+        print('level{0}'.format(level))
     elif (err == 0 and answer == 'N') or (err != 0 and answer == 'Y' ):
-        print(''' you're wrong ''')
+        print('\n','*'*20, 'sorry, you have to restart','*'*20)
         break
