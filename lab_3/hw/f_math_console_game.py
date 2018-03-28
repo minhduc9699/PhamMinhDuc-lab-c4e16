444f402842ccf42de306f52d2ed44ecd6f8e7364
minh duc <minhduc.096.99@gmail.com>
2018-03-28 12:44:50 +0700

    update

--- lab_3/hw/f_math_console_game.py
+++ lab_3/hw/f_math_console_game.py
@@ -2,24 +2,35 @@ from random import randint, choice
 from eval import calc
 level = 1
 playing = True
+print('\n')
+print('*'*20 ,'Welcome to Fmath game', '*'*20)
+print('\n')
+start = input('Pess Enter to play')
 while playing:
-    if level < 5:
+    if level < 4:
         x = randint(1, 10)
         y = randint(1, 10)
         err = randint(-1, 1)
-        op = ['+', '-', '*', '/', '+', '+', "+",]
+        op = ['+']
         ran_op = choice(op)
-    elif level >= 5:
-        if level == 5:
+    elif level <= 8:
+        if level == 4:
             print('level up!!')
-        x = randint(1, 25)
-        y = randint(1, 25)
+        x = randint(1, 20)
+        y = randint(1, 20)
         err = randint(-1, 1)
-        op = ['+', '+', '-', '-', '/', '/', "/", '/', '*', '*', '*']
+        op = ['+', '-']
         ran_op = choice(op)
-        if level == 10:
-            print('Win cmnr!')
-            break
+    elif level >= 12:
+        if level == 12:
+            print('level up!!!')
+        x = randint(1, 30)
+        y = randint(1, 30)
+        err = randint(-1, 1)
+        op = ['+', '-', '*', '/']
+        ran_op = choice(op)
+        if level == 20:
+            print('Win cmnr!!')
 
 
 
@@ -27,9 +38,9 @@ while playing:
 
 
 
-    print('{0} {1} {2} = {3}'.format(x, ran_op ,y, result))
+    print('*'*20,'{0} {1} {2} = {3}'.format(x, ran_op ,y, result), '*'*20)
 
-    answer = input('(Y/N)? ').upper()
+    answer = input('(Y/N)?').upper()
 
 
     if (err == 0 and answer == 'Y') or (err != 0 and answer == 'N'):
